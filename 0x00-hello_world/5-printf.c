cd2057f9d3a19e4e62fec1503ebf46f28282b47d
karemyon <karem.yon@gmail.com>
2023-06-19 17:50:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,11 @@
  * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
+ *
 */
 
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
